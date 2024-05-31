50f156766044b25d639773f0c56e1e12665e43d3
axltavideus <yeetna01@gmail.com>
2024-05-31 21:52:04 +0700

    new post card and index of posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,21 +8,25 @@
   </head>
 </head>
 <body>
-    <div class="card">
-        <div class="card-header">
-            Make a New post!
+    <div class="container d-flex flex-column">
+        <div class="d-table-cell align-middle pt-3">
+            <div class="card">
+                <div class="card-header">
+                    Make a New post!
+                </div>
+
+                <div class="card-body">
+                    <h5 class="card-title">Make a new blog post</h5>
+                    <p class="card-text">Press the button below to make a new blog post</p>
+                    <a href="{{ route('posts.create') }}" class="btn btn-primary">New Post</a>
+                    @if ($message = Session::get('success'))
+                        <p>{{ $message }}</p>
+                    @endif
+                </div>
+            </div>
         </div>
 
-        <div class="card-body">
-            <h5 class="card-title">Make a new blog post</h5>
-            <p class="card-text">Press the button below to make a new blog post</p>
-            <a href="{{ route('posts.create') }}" class="btn btn-primary">New Post</a>
-            @if ($message = Session::get('success'))
-                <p>{{ $message }}</p>
-            @endif
-        </div>
     </div>
-    
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
