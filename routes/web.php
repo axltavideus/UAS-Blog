77c8e1324042edc25ae4c3939697fdaeef01bc6d
RandyParker7 <randyestevan887@gmail.com>
2024-05-31 20:52:44 +0700

    web php nih

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/post', function () {
+    return view('post');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
