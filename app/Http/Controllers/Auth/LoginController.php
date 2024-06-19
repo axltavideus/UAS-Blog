30844f9aa147ba3dcb0c2f2c01bffc6c5d149e95
Stevericky <stevericky777@gmail.com>
2024-06-19 13:03:59 +0700

    perbaikin merge main

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
     {
         Auth::logout();
         $request->session()->invalidate();
-        $request->session()->regenarateToken();
+        $request->session()->regenerateToken();
 
         return redirect('/');
     }
@@ -38,5 +38,4 @@ class LoginController extends Controller
         $user = Auth::user();
         return view('profile', compact('user'));
     }
-
 }
